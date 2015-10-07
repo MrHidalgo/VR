677fcb7920f4d471e099fc6abcc40ebcbb6f98a4
MrHidalgo <Luckyhidalgo@gmail.com>
2015-10-07 15:58:48 +0300

    Left brand nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+/*
+* BTN HIDE LEFT MENU
+*/
+$('.hide-left-menu').click(function () {
+    $('.navbar-brand').toggleClass('slide-left').animate({
+        transition: '.5s'
+    });
+    $('.brand-logo').toggle('slow');
+    $('.icon-arrow-left').toggleClass('icon-arrow-right').animate({
+        transition: '.5s'
+    });
+});
