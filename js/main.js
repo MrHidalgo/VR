dbee9dfd42a9029c221097106261af6a77d8157c
MrHidalgo <Luckyhidalgo@gmail.com>
2015-10-08 11:36:04 +0300

    Replace min boot js/css  + Bg + Del file + Menu

--- js/main.js
+++ js/main.js
@@ -9,5 +9,28 @@ $('#screen').click(function(){
     ).animate({
         opacity: '1'
     }, 1500);
-   $(this).slideUp('1500');
+   $(this).slideUp('2000');
+});
+
+
+
+/*
+ *============================================
+ * SLIDE LEFT/RIGHT MENU
+ *============================================
+ */
+$('.arrow-swap, .arrow-swap').click(function(){
+    $('.navbar-brand').toggleClass('slide-left-menu').animate({
+        transition: '.5s'
+    });
+    $('.navbar-brand-text').toggle('fast');
+    $('.navbar-container').toggleClass('left-side-container').animate({
+        transition: '.5s'
+    });
+    $('.icon-arrow').toggleClass('icon-arrow-rotate').animate({
+        transition: '.5s'
+    });
+    $('.arrow-swap').toggleClass('btn-left').animate({
+        transition: '.5s'
+    });
 });
